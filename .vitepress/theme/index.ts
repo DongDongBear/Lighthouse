477f0bc343412b01b138210e9d91ab820fafce7d
DongDongBear <turing.nasarocket@gmail.com>
2026-03-03 15:44:47 +0800

    refactor(layout): implement new layout system with dedicated home and doc classes

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import ChatWidget from './components/ChatWidget.vue'
-import { h } from 'vue'
+import Layout from './components/Layout.vue'
 
 export default {
   extends: DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () => h(ChatWidget)
-    })
-  }
+  Layout
 }
